b6560af2685cfd85b9fe481bcf8b7bd8529685bc
Rational <2760535020@qq.com>
2026-03-05 21:05:53 +0800

    重新提交ArrayList

---
+++ ArrayList/src/PosIllegalityException.java
@@ -0,0 +1,8 @@
+public class PosIllegalityException extends RuntimeException{
+    public PosIllegalityException() {
+    }
+
+    public PosIllegalityException(String message) {
+        super(message);
+    }
+}
