483bd4363b707f638e0eca38aaec6594bc9cf981
Rational <2760535020@qq.com>
2026-03-05 21:05:14 +0800

    删除ArrayList文件

--- ArrayList/src/EmptyListException.java
+++
@@ -1,7 +0,0 @@
-public class EmptyListException extends RuntimeException{
-    public EmptyListException(){};
-
-    public EmptyListException(String message) {
-        super(message);
-    }
-}
--- ArrayList/src/PosIllegalityException.java
+++
@@ -1,8 +0,0 @@
-public class PosIllegalityException extends RuntimeException{
-    public PosIllegalityException() {
-    }
-
-    public PosIllegalityException(String message) {
-        super(message);
-    }
-}
