219bbb0b7680983eaba13abae85c30d16afd7435
Rational <2760535020@qq.com>
2026-03-05 20:53:51 +0800

    新增ArrayList

---
+++ ArrayList/src/Constant.java
@@ -0,0 +1,11 @@
+public class Constant {
+
+    public static final String EMPTY_LIST = "顺序表为空";
+    public static final int DEFAULT_CAPACITY = 10;
+
+    public static final String GET_POS_ILLEGALITY = "get方法的pos值不合法";
+
+    public static final String SET_POS_ILLEGALITY = "set方法的pos值不合法";
+
+    public static final String ADD_POS_ILLEGALITY = "add方法的pos值不合法";
+}
